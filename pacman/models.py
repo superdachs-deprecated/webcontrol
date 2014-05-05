166f63bfa25503054b4d5881ac8479773ed4c103
Stefan Kauerauf <mail@stefankauerauf.de>
2014-05-05 17:31:04 +0200

    include south migration framework

--- pacman/models.py
+++ pacman/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Repository(models.model):
+    name = models.CharField(max_length=10)
+    url = models.URLField(max_length=200)
+    isActive = models.BooleanField()
--- webcontrol/settings.py
+++ webcontrol/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'network_base',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
