181fb6a77407419d621514b66beb48dad10e7ef0
Stefan Kauerauf <mail@stefankauerauf.de>
2014-05-05 17:59:13 +0200

    removed south

--- pacman/models.py
+++ pacman/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class Repository(models.model):
+class Distribution(models.Model):
+    name = models.CharField(max_length=40)
+
+class Repository(models.Model):
     name = models.CharField(max_length=10)
     url = models.URLField(max_length=200)
     isActive = models.BooleanField()
+    distribution = models.ForeignKey(Distribution)
--- webcontrol/settings.py
+++ webcontrol/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
+    'network_base',
+    'ntp',
+    'pacman',
+    'setup',
+    'ssh',
 )
 
 MIDDLEWARE_CLASSES = (
